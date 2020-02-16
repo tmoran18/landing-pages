e711ae245e7c1616feaa1600b9fd4d0f27a2ee6d
tmoran18 <moran.tim01@gmail.com>
2020-02-16 21:29:52 +1000

    Initial setup with SCSS and compoents complete

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+import "../styles/styles.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hellow World</h1>
+      <img src="/images/green.png" alt="" />
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
